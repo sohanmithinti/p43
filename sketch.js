155e02b84bc9f2e0fae64d42870576f5318f3400
sohanmithinti <63795028+sohanmithinti@users.noreply.github.com>
2020-07-06 20:15:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+Game Design Document
+Fill up the Following document 
+
+________________________________________
+
+
+
+1.	Write the title of your project.
+A.	Trex Game
+________________________________________
+
+
+2.	What is the goal of the game? 
+A.	To go as far as possible
+________________________________________
+
+
+3.	Write a brief story of your game?
+A.	Because the world is going to end.
+________________________________________
+
+________________________________________
+
+________________________________________
+
+
+ 
+4.	Which are the playing characters of this game? 
+●	Playing characters are the ones which respond to the user based on the input from the user
+●	Cars, monkeys, dinos, wizards etc, are the playing characters in the game.  
+Number	Character Name	What can this character do? 
+1	dino	running
+2	obstacles	To aviod trex from running
+3	clouds	To look like original
+4	ground	Makes trex to run
+5	Invisible ground	Aviod trex from falling
+6		
+7		
+8		
+ 
+6.	Which are the Non Playing Characters of this game?
+●	Non Playing characters are the ones which don't have an action or a behaviour when the user interacts with the game.
+●	Hurdles, stones, bananas, coins, etc are non playing characters in the game.   
+Number	Character Name	What can this character do? 
+1	dino	running
+2	obstacles	To aviod trex from running
+3	clouds	To look like original
+4	ground	Makes trex to run
+5	Invisible ground	Aviod trex from falling
+6		
+7		
+8		
+
+
+
+Draw your imagination of this game. What does this game look like?
+●	Draw the game either on your computer or on paper, 
+●	Add images of scenes of the game to show each of the playing and non-playing characters at least once.  
+
+
+________________________________________
+________________________________________
+________________________________________
+________________________________________
+
+How do you plan to make your game engaging? 
+By making it more difficult after certain frames.________________________________________________________________________________________________________________________
+
+
+
